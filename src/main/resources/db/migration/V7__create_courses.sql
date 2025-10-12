6dab4d890693de4007a1b63c84b14237b966f3d9
SugAr <kkwandy@naver.com>
2025-10-12 22:57:51 +0900

    fix typo of V7

--- src/main/resources/db/migration/V7__create_courses.sql
+++ src/main/resources/db/migration/V7__create_courses.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS course
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    year INT NOT NULL,
+    'year' INT NOT NULL,
     semester VARCHAR(16) NOT NULL,
     category VARCHAR(64) NULL,
     college VARCHAR(64) NULL,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS course
     professor VARCHAR(64) NULL,
     room VARCHAR(64) NULL,
 
-    UNIQUE KEY unique_course(year, semester, courseNumber, classNumber),
+    UNIQUE KEY unique_course(year, semester, courseNumber, classNumber)
 )
 
 CREATE TABLE IF NOT EXISTS course_time(
@@ -26,14 +26,14 @@ CREATE TABLE IF NOT EXISTS course_time(
     end_min INT NOT NULL,
     location VARCHAR(64) NULL,
 
-    CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE,
+    CONSTRAINT fk_course_time_w_course FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
 )
 
 CREATE TABLE IF NOT EXISTS timetable(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    year INT NOT NULL,
+    'name' VARCHAR(64) NOT NULL,
+    'year' INT NOT NULL,
     semester VARCHAR(16) NOT NULL,
 
     UNIQUE KEY unique_timetable(user_id, year, semester, name)
@@ -44,8 +44,8 @@ CREATE TABLE IF NOT EXISTS timetable_course(
     timetable_num BIGINT NOT NULL,
     course_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_timetable_num FOREIGN KEY (timetable_num) REFERENCES timetable(id) ON DELETE CASCADE,
-    CONSTRAINT fk_course_id FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE,
+    CONSTRAINT fk_timetable_course_w_timetable FOREIGN KEY (timetable_num) REFERENCES timetable(id) ON DELETE CASCADE,
+    CONSTRAINT fk_timetable_course_w_course FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE,
 
     UNIQUE KEY unique_timetable_course(timetable_num, course_id)
 )
