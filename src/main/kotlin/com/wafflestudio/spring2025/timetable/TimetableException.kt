00b08ec4fc0a82fe97514f6e15652ee262cb8f98
Lee Seunghyeon <lsh09130@gmail.com>
2025-10-29 16:03:27 +0900

    Make timetable folder

---
+++ src/main/kotlin/com/wafflestudio/spring2025/timetable/TimetableException.kt
@@ -0,0 +1,35 @@
+package com.wafflestudio.spring2025.timetable
+
+import com.wafflestudio.spring2025.DomainException
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+
+// @TODO
+
+sealed class BoardException(
+    errorCode: Int,
+    httpStatusCode: HttpStatusCode,
+    msg: String,
+    cause: Throwable? = null,
+) : DomainException(errorCode, httpStatusCode, msg, cause)
+
+class BoardNotFoundException :
+    BoardException(
+        errorCode = 0,
+        httpStatusCode = HttpStatus.NOT_FOUND,
+        msg = "Board not found",
+    )
+
+class BoardNameBlankException :
+    BoardException(
+        errorCode = 0,
+        httpStatusCode = HttpStatus.BAD_REQUEST,
+        msg = "Board name is blank",
+    )
+
+class BoardNameConflictException :
+    BoardException(
+        errorCode = 0,
+        httpStatusCode = HttpStatus.CONFLICT,
+        msg = "Board name already exists",
+    )
---
+++ src/main/kotlin/com/wafflestudio/spring2025/timetable/controller/TimetableController.kt
@@ -0,0 +1,34 @@
+package com.wafflestudio.spring2025.timetable.controller
+
+// @TODO
+
+import com.wafflestudio.spring2025.board.dto.CreateBoardRequest
+import com.wafflestudio.spring2025.board.dto.CreateBoardResponse
+import com.wafflestudio.spring2025.board.dto.ListBoardResponse
+import com.wafflestudio.spring2025.board.service.BoardService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/boards")
+class TimetableController(
+    private val boardService: BoardService,
+) {
+    @PostMapping
+    fun create(
+        @RequestBody createRequest: CreateBoardRequest,
+    ): ResponseEntity<CreateBoardResponse> {
+        val board = boardService.create(createRequest.name)
+        return ResponseEntity.ok(board)
+    }
+
+    @GetMapping
+    fun list(): ResponseEntity<ListBoardResponse> {
+        val boards = boardService.list()
+        return ResponseEntity.ok(boards)
+    }
+}
