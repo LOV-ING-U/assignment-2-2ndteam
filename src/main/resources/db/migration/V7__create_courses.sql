10d8d9d9b3aa11fb8f34d049d4aa8a7961bcac26
Lee Seunghyeon <lsh09130@gmail.com>
2025-10-29 15:59:47 +0900

    Separate DBs

--- src/main/resources/db/migration/V7__create_courses.sql
+++ src/main/resources/db/migration/V7__create_courses.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS course
+CREATE TABLE IF NOT EXISTS courses
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `year` INT NOT NULL,
+    year INT NOT NULL,
     semester VARCHAR(16) NOT NULL,
     category VARCHAR(64) NULL,
     college VARCHAR(64) NULL,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS course
     room VARCHAR(64) NULL,
 
     UNIQUE KEY unique_course(year, semester, courseNumber, classNumber)
-)
+);
 
 CREATE TABLE IF NOT EXISTS course_time(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -28,25 +28,4 @@ CREATE TABLE IF NOT EXISTS course_time(
     location VARCHAR(64) NULL,
 
     CONSTRAINT fk_course_time_w_course FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE
-)
-
-CREATE TABLE IF NOT EXISTS timetable(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    `name` VARCHAR(64) NOT NULL,
-    `year` INT NOT NULL,
-    semester VARCHAR(16) NOT NULL,
-
-    UNIQUE KEY unique_timetable(user_id, year, semester, name)
-)
-
-CREATE TABLE IF NOT EXISTS timetable_course(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    timetable_num BIGINT NOT NULL,
-    course_id BIGINT NOT NULL,
-
-    CONSTRAINT fk_timetable_course_w_timetable FOREIGN KEY (timetable_num) REFERENCES timetable(id) ON DELETE CASCADE,
-    CONSTRAINT fk_timetable_course_w_course FOREIGN KEY (course_id) REFERENCES course(id) ON DELETE CASCADE,
-
-    UNIQUE KEY unique_timetable_course(timetable_num, course_id)
-)
+);
