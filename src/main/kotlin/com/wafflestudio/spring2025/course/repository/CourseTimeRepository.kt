1921105344a41844f93c80161ec3d1756bfe1b7a
seulchan-lee <leeseulchan@snu.ac.kr>
2025-10-31 14:57:23 +0900

    add CourseTimeRepository.kt

--- src/main/kotlin/com/wafflestudio/spring2025/course/repository/CourseTimeRepository.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/course/repository/CourseTimeRepository.kt
@@ -1,4 +1,12 @@
 package com.wafflestudio.spring2025.course.repository
 
-interface CourseTimeRepository {
+import com.wafflestudio.spring2025.course.model.CourseTime
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
+
+interface CourseTimeRepository : CrudRepository<CourseTime, Long> {
+
+    @Query("SELECT * FROM course_time WHERE course_id IN (:ids)")
+    fun findByCourseIds(@Param("ids") ids: List<Long>): List<CourseTime>
 }
