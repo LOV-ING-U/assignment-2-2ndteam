7ecaf656512ba602b79d816e60b5892dcacae74f
seulchan-lee <leeseulchan@snu.ac.kr>
2025-10-31 14:37:39 +0900

    add SearchCourseRequest.kt, SearchCourseResponse.kt

--- src/main/kotlin/com/wafflestudio/spring2025/course/dto/course/SearchCourseRequest.kt
+++ src/main/kotlin/com/wafflestudio/spring2025/course/dto/course/SearchCourseRequest.kt
@@ -1,4 +1,9 @@
 package com.wafflestudio.spring2025.course.dto.course
 
-class SearchCourseRequest {
-}
+data class SearchCourseRequest (
+    val year: Int,
+    val semester: String,
+    val keyword: String? = null,
+    val limit: Int = 20,
+    val nextId: Long? = null
+)
